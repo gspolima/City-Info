4a952b1b45a32f4222659db6b1ab4d331907125f
Gustavo Sampaio <sampaioglima@gmail.com>
2021-06-14 20:34:45 -0300

    Add the first two resources of the API.

---
+++ src/CityInfo.API/Controllers/CitiesController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace CityInfo.API.Controllers
+{
+    [ApiController]
+    [Route("api/cities")]
+    public class CitiesController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetCities()
+        {
+            return Ok(CitiesDataStore.Current.Cities);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetCityById(int id)
+        {
+            var city = CitiesDataStore.Current.Cities
+                .SingleOrDefault(c => c.Id == id);
+
+            if (city == null)
+                return NotFound();
+
+            return Ok(city);
+        }
+    }
+}
