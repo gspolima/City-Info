4e3cb06d8bc182908fec0ad7df2f0a6617391605
Gustavo Sampaio <sampaioglima@gmail.com>
2021-06-14 20:31:38 -0300

    Mock a data access layer with in-memory content.

---
+++ src/CityInfo.API/CitiesDataStore.cs
@@ -0,0 +1,36 @@
+﻿using CityInfo.API.Models;
+using System.Collections.Generic;
+
+namespace CityInfo.API
+{
+    public class CitiesDataStore
+    {
+        public static CitiesDataStore Current => new CitiesDataStore();
+        public List<CityDto> Cities { get; set; }
+
+        public CitiesDataStore()
+        {
+            Cities = new List<CityDto>()
+            {
+                new CityDto()
+                {
+                    Id = 1,
+                    Name = "Liverpool",
+                    Description = "A fairly well-known rock n roll band was born there"
+                },
+                new CityDto()
+                {
+                    Id = 2,
+                    Name = "Paris",
+                    Description = "The city with a gigantic tower."
+                },
+                new CityDto()
+                {
+                    Id = 3,
+                    Name = "Antwerp",
+                    Description = "The city with a cathedral that was never really finished"
+                }
+            };
+        }
+    }
+}
