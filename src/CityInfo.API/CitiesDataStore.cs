e17e1b0073ca4105906fb4eb2e5d0d576f26a92f
Gustavo Sampaio <sampaioglima@gmail.com>
2021-06-14 21:50:04 -0300

    Add Points of Interest to the Cities.

--- src/CityInfo.API/CitiesDataStore.cs
+++ src/CityInfo.API/CitiesDataStore.cs
@@ -16,19 +16,64 @@ namespace CityInfo.API
                 {
                     Id = 1,
                     Name = "Liverpool",
-                    Description = "A fairly well-known rock n roll band was born there"
+                    Description = "A fairly well-known rock n roll band was born there.",
+                    PointsOfInterest = new List<PointOfInterestDto>()
+                    {
+                        new PointOfInterestDto()
+                        {
+                            Id = 1,
+                            Name = "The Beatles Story",
+                            Description = "The world’s largest permanent exhibition purely devoted to telling the story of The Beatles’ rise to fame."
+                        },
+                        new PointOfInterestDto()
+                        {
+                            Id = 2,
+                            Name = "Sefton Park",
+                            Description = "A magnificent 200-acre Park that looks like a natural landscape rather than a man-made park."
+                        }
+                    }
                 },
                 new CityDto()
                 {
                     Id = 2,
                     Name = "Paris",
-                    Description = "The city with a gigantic tower."
+                    Description = "The city with a gigantic tower.",
+                    PointsOfInterest = new List<PointOfInterestDto>()
+                    {
+                        new PointOfInterestDto()
+                        {
+                            Id = 1,
+                            Name = "Louvre Museum",
+                            Description = "Landmark art museum with vast collection."
+                        },
+                        new PointOfInterestDto()
+                        {
+                            Id = 2,
+                            Name = "Arc de Triomphe",
+                            Description = "Triumphal arch and national monument."
+                        }
+                    }
                 },
                 new CityDto()
                 {
                     Id = 3,
                     Name = "Antwerp",
-                    Description = "The city with a cathedral that was never really finished"
+                    Description = "The city with a cathedral that was never really finished.",
+                    PointsOfInterest = new List<PointOfInterestDto>()
+                    {
+                        new PointOfInterestDto()
+                        {
+                            Id = 1,
+                            Name = "Grand Place",
+                            Description = "The Grote Markt with its town hall and numerous guild houses is the heart of the old town."
+                        },
+                        new PointOfInterestDto()
+                        {
+                            Id = 2,
+                            Name = "Cathedral of Our Lady",
+                            Description = "Belgium's largest Gothic church."
+                        }
+                    }
                 }
             };
         }
--- src/CityInfo.API/Models/CityDto.cs
+++ src/CityInfo.API/Models/CityDto.cs
@@ -1,9 +1,15 @@
-﻿namespace CityInfo.API.Models
+﻿using System.Collections.Generic;
+
+namespace CityInfo.API.Models
 {
     public class CityDto
     {
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+        public int NumberOfPointsOfInterest => PointsOfInterest.Count;
+        public ICollection<PointOfInterestDto> PointsOfInterest { get; set; } =
+            new List<PointOfInterestDto>();
+
     }
 }
