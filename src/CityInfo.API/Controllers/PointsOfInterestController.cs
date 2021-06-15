71f8f1e142ff3d5f0bb36c73043ab74d1f50b266
Gustavo Sampaio <sampaioglima@gmail.com>
2021-06-14 21:50:45 -0300

    Add resources to interact with points of interest.

---
+++ src/CityInfo.API/Controllers/PointsOfInterestController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace CityInfo.API.Controllers
+{
+    [ApiController]
+    [Route("api/cities/{cityId}/pointsofinterest")]
+    public class PointsOfInterestController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetPointsOfInterest(int cityId)
+        {
+            var city = CitiesDataStore.Current.Cities
+                .SingleOrDefault(c => c.Id == cityId);
+
+            if (city == null)
+                return NotFound();
+
+            return Ok(city.PointsOfInterest);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetPointOfInterestById(int cityId, int id)
+        {
+            var city = CitiesDataStore.Current.Cities
+                .SingleOrDefault(c => c.Id == cityId);
+
+            if (city == null)
+                return NotFound();
+
+            var pointOfInterest = city.PointsOfInterest
+                .SingleOrDefault(p => p.Id == id);
+
+            if (pointOfInterest == null)
+                return NotFound();
+
+            return Ok(pointOfInterest);
+        }
+    }
+}
